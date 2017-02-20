aaf2cf1e7433670ea85b1c66c3e222112a67c85c
manuelRod <manuel.rodriguez.rosado@gmail.com>
2017-02-20 17:56:46 +0100

    Adding unregister configurations

--- src/yaml-configurator/controllers/mainController.php
+++ src/yaml-configurator/controllers/mainController.php
@@ -25,7 +25,7 @@ class mainController {
      */
     function registerConfigurationFiles() {
         if (!$this->checkScope()) {
-            //return;
+            return;
         }
 
         $registered_results = array();
@@ -46,43 +46,109 @@ class mainController {
                 }
             }
         }
-        flush_rewrite_rules();
+        $this->afterRegisterProcess($registered_results);
+    }
+
+    function unregisterConfigurationFiles($deleted_configurations)
+    {
+        foreach ($deleted_configurations as $config_type => $files) {
+            foreach ($files as $file_name => $value) {
+                /**
+                 * Check which type of config type
+                 */
+                if ($config_type == 'post_types') {
+                    new unregisterCustomPostType();
+                    (new unregisterCustomPostType())->unregister($file_name);
+                }
+            }
+        }
+    }
+
+    /**
+     * After Register process
+     * 1. Check if any deleted configuration
+     * 2. Unregister Deleted Configuration
+     * 3. Save new registered options
+     * 4. Flush Rules
+     * 5. Print result on screen
+     *
+     *
+     * @param $registered_results
+     */
+    protected function afterRegisterProcess($registered_results)
+    {
+        $deleted_configurations = $this->checkDeletedConfigurations($registered_results);
+        $this->unregisterConfigurationFiles($deleted_configurations);
+
         $this->saveRegisteredOptions($registered_results);
-        $this->printFriendlyResult($registered_results);
+        flush_rewrite_rules();
+        $this->printFriendlyResult($registered_results, $deleted_configurations);
+    }
+
+
+    /**
+     * Compare Registered Custom Types from DB with new $registed_results, differences will be returned.
+     *
+     * @param array $registered_results
+     * @return array
+     */
+    protected function checkDeletedConfigurations($registered_results) {
+        $diffs = array();
+        $options = get_option(YAML_OPTIONS);
+        foreach ($options as $custom_type => $custom_type_registered_names) {
+            foreach ($custom_type_registered_names as $ctrn => $value) {
+                if (!isset($registered_results[$custom_type][$ctrn])) {
+                    $diffs[$custom_type][$ctrn] = true;
+                }
+            }
+        }
+        return $diffs;
     }
 
     /**
      * Saves on Options the registered custom types
      *
-     * @param $registered_results
+     * @param array $registered_results
+     * @return bool
      */
     protected function saveRegisteredOptions($registered_results) {
-        $options = array();
-        foreach ($registered_results as $registered_type => $values) {
-            foreach ($values as $name => $value) {
-                if ($value) {
-                    $options[$registered_type][] = $name;
-                }
-            }
-        }
-        update_option(YAML_OPTIONS, $options, false);
+        return update_option(YAML_OPTIONS, $registered_results, false);
     }
 
     /**
      * Prints a friendly log for the user when finished.
      *
-     * @param $registered_results
+     * @param array $registered_results
+     * @param array $deleted_configurations
      */
-    protected function printFriendlyResult($registered_results) {
+    protected function printFriendlyResult($registered_results, $deleted_configurations) {
         $string = '<h1>Thanks for using YAML Wordpress Dynamic Content Type Configurator</h1>';
         $string .= '<h2>Registration output logs</h2>';
-        foreach ($registered_results as $registered_type => $values) {
-            $string .= "<h2>$registered_type :</h2>";
-            foreach ($values as $name => $value) {
-                $success = ($value) ? 'registered' : 'error';
-                $string .= "<p>$name : $success</p>";
+        if (empty($registered_results)) {
+            $string .= "<p>Nothing to register</p>";
+        } else {
+            foreach ($registered_results as $registered_type => $values) {
+                $string .= "<h2>$registered_type :</h2>";
+                foreach ($values as $name => $value) {
+                    $success = ($value) ? 'registered' : 'error';
+                    $string .= "<p>$name : $success</p>";
+                }
             }
         }
+
+        $string .= '<h2>Unregistered output logs</h2>';
+        if (empty($deleted_configurations)) {
+            $string .= "<p>Nothing to Unregister</p>";
+        } else {
+            foreach ($deleted_configurations as $registered_type => $values) {
+                $string .= "<h2>$registered_type :</h2>";
+                foreach ($values as $name => $value) {
+                    $success = ($value) ? 'unregistered' : 'error';
+                    $string .= "<p>$name : $success</p>";
+                }
+            }
+        }
+
         echo $string; die;
     }
 
